8592cc6386b23d8fc2e57e4f99212dc39ed87adb
Dominik Korsa <dominik.korsa@gmail.com>
2019-10-29 22:57:53 +0100

    Add contact form

--- src/components/HomeContactFormSection.vue
+++ src/components/HomeContactFormSection.vue
@@ -1,17 +1,35 @@
 <template>
-  <div class="contact-form home-section">
-    <h2 class="contact-form__title">Kontakt</h2>
-    <div class="contact-form__direct">
-      <a class="contact-form__direct-link" href="mailto:wulkanowyinc@gmail.com" title="Email">
+  <div class="contact home-section">
+    <h2 class="contact__title">Kontakt</h2>
+    <div class="contact__direct">
+      <a class="contact__direct-link" href="mailto:wulkanowyinc@gmail.com" title="Email">
         <span class="mdi mdi-email"></span>
         <span class="text">wulkanowyinc@gmail.com</span>
       </a>
-      <a class="contact-form__direct-link" href="https://discord.gg/vccAQBr" title="Discord">
+      <a class="contact__direct-link" href="https://discord.gg/vccAQBr" title="Discord">
         <span class="mdi mdi-discord"></span>
         <span class="text">wulkanowy</span>
       </a>
     </div>
-    <form>
+    <form class="contact__form" action="https://formspree.io/xqkzgzqp" method="POST">
+      <label for="contact__form-email" class="contact__form-label">Twój adres email:</label>
+      <input
+        type="email"
+        id="contact__form-email"
+        class="contact__form-email"
+        name="_replyto"
+        required
+        placeholder="twój-email@example.com"
+      >
+      <label for="contact__form-message" class="contact__form-label">Wiadomość:</label>
+      <textarea
+        name="message"
+        id="contact__form-message"
+        class="contact__form-message"
+        required
+        placeholder="Twoja wiadomość"
+      ></textarea>
+      <input type="submit" class="contact__form-submit" value="Wyślij">
     </form>
   </div>
 </template>
@@ -23,13 +41,13 @@
 </script>
 
 <style lang="scss">
-  .contact-form {
+  .contact {
     height: 100vh;
     box-sizing: border-box;
     padding: 0 32px 32px;
     overflow: hidden;
     display: grid;
-    grid-template-columns: 1fr auto 1fr;
+    grid-template-columns: 1fr auto auto 1fr;
     grid-template-rows: auto 1fr auto 1fr;
     background:
       linear-gradient(
@@ -43,6 +61,7 @@
 
     @media screen and (max-width: 875px) {
       grid-template-columns: 1fr;
+      grid-template-rows: auto auto 1fr auto 1fr;
       padding: 0 16px 16px;
     }
 
@@ -56,7 +75,7 @@
       color: #fff;
       font-family: 'Roboto', sans-serif;
       font-weight: 400;
-      grid-column: 1/4;
+      grid-column: 1/5;
       grid-row: 1;
 
       @media screen and (max-width: 875px) {
@@ -67,10 +86,13 @@
     &__direct {
       grid-column: 2;
       grid-row: 3;
-      padding: 8px;
+      padding: 8px 32px;
+      align-self: center;
 
       @media screen and (max-width: 875px) {
         grid-column: 1;
+        grid-row: 2;
+        justify-self: center;
       }
     }
 
@@ -95,6 +117,13 @@
       flex-direction: row;
       align-items: center;
 
+      @media screen and (max-width: 450px) {
+        padding: 4px;
+        padding-left: 8px;
+        padding-right: 10px;
+        height: 32px;
+      }
+
       &:focus {
         outline: none;
       }
@@ -109,11 +138,109 @@
         font-size: 20px;
         flex-grow: 1;
         text-align: center;
+
+        @media screen and (max-width: 450px) {
+          font-size: 16px;
+        }
       }
 
       .mdi {
         font-size: 32px;
         margin-right: 10px;
+
+        @media screen and (max-width: 450px) {
+          font-size: 28px;
+          margin-right: 8px;
+        }
+      }
+    }
+
+    &__form {
+      display: flex;
+      padding: 8px 32px;
+      grid-row: 3;
+      grid-column: 3;
+      flex-direction: column;
+      justify-content: center;
+      align-items: stretch;
+      min-width: 30vw;
+
+      @media screen and (max-width: 875px) {
+        grid-column: 1;
+        grid-row: 4;
+      }
+    }
+
+    .contact__form-label {
+      color: #fff;
+      text-align: center;
+      margin: 8px 16px;
+      font-size: 20px;
+      font-family: 'Roboto', sans-serif;
+    }
+
+    &__form-email, &__form-message {
+      background: none;
+      border: none;
+      color: #fff;
+      font-size: 20px;
+      border: 1px solid #fff;
+      border-radius: 5px;
+      padding: 4px 8px;
+      transition: background-color 300ms;
+
+      &::placeholder {
+        color: #fffa;
+        font-weight: 300;
+        font-family: 'Roboto Mono', monospace;
+      }
+
+      &:focus, &:hover {
+        padding: 3px 7px;
+        border: 2px solid #fff;
+      }
+
+      &:hover {
+        background-color: #fff2;
+      }
+
+      &:focus {
+        outline: none;
+        background-color: #fff5;
+      }
+    }
+
+    &__form-email {
+      font-family: 'Roboto Mono', monospace;
+    }
+
+    &__form-message {
+      font-family: 'Roboto', sans-serif;
+      flex-grow: 1;
+      resize: vertical;
+      min-height: 120px;
+    }
+
+    &__form-submit {
+      background: #fff;
+      border: none;
+      font-size: 20px;
+      border-radius: 4px;
+      padding: 8px 8px;
+      transition: background-color 300ms;
+      font-family: 'Roboto', sans-serif;
+      margin-top: 16px;
+      cursor: pointer;
+      min-height: 48px;
+
+      &:focus {
+        outline: none;
+      }
+
+      &:focus, &:hover {
+        box-shadow: 0 11px 15px -7px rgba(0, 0, 0, 0.2),
+          0 24px 38px 3px rgba(0, 0, 0, 0.14),
+          0 9px 46px 8px rgba(0, 0, 0, 0.12);
       }
     }
   }
